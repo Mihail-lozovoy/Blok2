c1f00b0274bf8dc166e53a5f87ca5afdf26f195e
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-06-11 16:38:03 +0300

    Add files via upload

---
+++ module_2_7.py
@@ -0,0 +1,25 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params()
+
+print_params(3, 4, 5)
+
+print_params(b=25)
+
+print_params(c=[1, 2, 3])
+
+
+values_list = [1, 'rrrrrr', True]
+values_dict = {'a': 2, 'b': 'ggggg', 'c': False}
+
+print_params(*values_list)
+
+print_params(**values_dict)
+
+print_params(*values_dict)
+
+
+values_list_2 = [54.32, 'Строка' ]
+
+print_params(*values_list_2, 42)

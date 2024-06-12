3c734185079bca04b59de32cd91879af3f5dcd53
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-06-12 22:12:06 +0300

    Update module_2_6.py

--- module_2_6.py
+++ module_2_6.py
@@ -1,11 +1,12 @@
-def single_root_words(root_word, *other_words):
+def single_root_words(root_words, *other_words):
+
     same_words = []
-    for word in other_words:
-        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
-            same_words.append(word.lower())
+
+    for i in other_words:
+        if root_words.lower() in i.lower() or i.lower() in root_words.lower():
+            same_words.append(i)
 
     return same_words
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
